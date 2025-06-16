c469a4f5858fd1e4b74b7aba6f8b66024ae9a205
mnene-william <mnenewilliam26@gmail.com>
2025-06-16 19:15:12 +0300

    Add Firebase integration and update dependencies
    - Added Firebase SDK as a dependency in package.json.
    - Created firebase.js to initialize Firebase with configuration and export auth and Firestore instances.

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOcQfJQPqOAllSGYS1UKG2-OgqW17cC1Q",
+  authDomain: "trekmate-127e9.firebaseapp.com",
+  projectId: "trekmate-127e9",
+  storageBucket: "trekmate-127e9.firebasestorage.app",
+  messagingSenderId: "754337651808",
+  appId: "1:754337651808:web:57de5bb020f71a9b65f705",
+  measurementId: "G-QE0WMV3FC0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
