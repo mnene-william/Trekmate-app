85bdc0908ac564b795cc17b999bf94ec7bb72c6a
mnene-william <mnenewilliam26@gmail.com>
2025-06-19 13:27:48 +0300

    feat: add navigation to homepage after successful trip creation

--- src/Components/CreateTrip.jsx
+++ src/Components/CreateTrip.jsx
@@ -53,6 +53,7 @@ function CreateTrip(){
 
         setSubmitLoading(false);
         alert("Trip created successfully!");
+        navigate('/homepage');
 
         setTripData({
             title: '',
